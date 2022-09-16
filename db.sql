35c6051262076275b41435ec37e051a49dd87265
Onkar More <93419853+OnkarMore3489@users.noreply.github.com>
2022-09-16 16:44:45 +0530

    Add files via upload

---
+++ db.sql
@@ -0,0 +1,3 @@
+drop table employee;
+
+create table employee(emp_id int,emp_name varchar(20),salary float);
---
+++ db1.sql
@@ -0,0 +1,4 @@
+
+drop table attendance;
+
+create table attendance(roll_no int,name varchar(20),subject varchar(20),total_att int);
